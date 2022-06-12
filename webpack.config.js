18da9102ed4606a2073c79c96805e894d0c7e328
Ian Welerson <ian.nawes@gmail.com>
2022-06-12 10:55:30 -0300

    build(webpack): add more webpack config and generate the first dist folder content

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // TODO: add this plugin to plugin array
 
 module.exports = (env) => {
@@ -9,25 +10,30 @@ module.exports = (env) => {
   return {
     mode: environment,
     devtool: environment === 'production' ? 'source-map' : 'eval-cheap-source-map',
-    entry: path.resolve(__dirname, './src/assets/js/entry.js'),
+    entry: path.resolve(__dirname, 'src', 'entry.js'),
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src/assets/')
+        '@': path.resolve(__dirname, 'src/assets/')
       }
     },
     output: {
-      path: path.resolve(__dirname, './dist'),
+      path: path.resolve(__dirname, 'dist'),
       assetModuleFilename: "[name].[hash][ext]",
       filename: 'main.[hash].js',
+      clean: true,
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.join(__dirname, './src/index.html'),
-        inject: 'body',
-        hash: true
+        template: path.join(__dirname, 'src', 'index.html'),
+        inject: 'body'
       }),
       new MiniCssExtractPlugin({
-        filename: '[name].[hash].css' //TODO: change this to dinamic
+        filename: '[name].[hash].css'
+      }),
+      new CopyPlugin({
+        patterns: [
+          path.resolve(__dirname, 'src', 'sitemap.xml'),
+        ]
       }),
     ],
     module: {
@@ -41,14 +47,18 @@ module.exports = (env) => {
           ],
         },
         {
-            test: /\.(html)$/,
-            use: ['html-loader']
-        }
+          test: /\.(html)$/,
+          use: ['html-loader']
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
     devServer: {
       static: {
-        directory: path.join(__dirname, './src'),
+        directory: path.join(__dirname, 'src'),
       },
       hot: true,
       port: 8000,
